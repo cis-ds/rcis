ba5ddbed881c08a1d08b0a27efe1ece643044e62
Benjamin Soltoff <ben@bensoltoff.com>
2016-08-08 14:23:34 -0500

    Change cfss_slides and cfss_notes functions to be called by `output` YAML option, rather than `knit` option

--- R/render.R
+++ R/render.R
@@ -2,47 +2,31 @@
 #'
 #' Function to generate slides for class.
 #'
-#' @param inputFile Rmd file to be rendered
-#' @param encoding
 #'
 #' @export
 #'
-cfss_slides <- function(inputFile, encoding) {
-  # call revealjs to generate output format
-  out_format <- revealjs::revealjs_presentation(theme = "simple",
-                                                highlight = "pygments",
-                                                incremental = TRUE,
-                                                transition = "fade",
-                                                center = FALSE,
-                                                # css = css,
-                                                reveal_options = list(center = FALSE,
-                                                                      previewLinks = TRUE))
-
-  # render using rmarkdown
-  rmarkdown::render(inputFile,
-                    encoding = encoding,
-                    output_format = out_format)
+cfss_slides <- function() {
+  revealjs::revealjs_presentation(theme = "simple",
+                                  highlight = "pygments",
+                                  incremental = TRUE,
+                                  transition = "fade",
+                                  center = FALSE,
+                                  # css = css,
+                                  reveal_options = list(center = FALSE,
+                                                        previewLinks = TRUE))
 }
 
 #' cfss_notes
 #'
 #' Function to generate notes for class.
 #'
-#' @param inputFile Rmd file to be rendered
-#' @param encoding
 #'
 #' @export
 #'
-cfss_notes <- function(inputFile, encoding) {
-  # call the base html_document function
-  out_format <- rmarkdown::html_document(theme = "simple",
+cfss_notes <- function() {
+  rmarkdown::html_document(theme = "readable",
                            highlight = "pygments",
                            toc = TRUE,
                            toc_float = TRUE)
-
-  # render using rmarkdown
-  rmarkdown::render(inputFile,
-                    encoding = encoding,
-                    output_format = out_format)
 }
 
