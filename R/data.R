7e571b61c955a8e305f7a48aef2faec89c2bc254
Benjamin Soltoff <ben@bensoltoff.com>
2016-09-21 10:44:15 -0500

    Add gun deaths dataset

--- R/data.R
+++ R/data.R
@@ -21,3 +21,25 @@
 #' }
 #' @source \url{https://collegescorecard.ed.gov/data/}
 "scorecard"
+
+#' Gun Deaths in America
+#'
+#' A dataset containing records on all firearm deaths in the United
+#' States (excluding non-U.S. residents) from 2012-2014.
+#'
+#' @format A data frame with 100,798 rows and 10 variables:
+#' \describe{
+#'   \item{id}{Row number of the observation}
+#'   \item{year}{Year the death occurred}
+#'   \item{month}{Month the death occurred (1 = January, 2 = February, etc.)}
+#'   \item{intent}{Intent of the death (homicide, suicide, accidental, unknown)}
+#'   \item{police}{Indicator if the death was the result of an officer-involved shooting}
+#'   \item{sex}{Victim's sex}
+#'   \item{age}{Victim's age}
+#'   \item{race}{Victim's race (White, Black, Hispanic, Asian/Pacific Islander,
+#'   Native American/Native Alaskan)}
+#'   \item{place}{Location where the shooting occurred}
+#'   \item{education}{Highest level of education of the victim}
+#' }
+#' @source \url{http://fivethirtyeight.com/features/gun-deaths/}
+"gun_deaths"
