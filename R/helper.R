f21005f0179e9535108e1aa92ad1957e817a0799
Benjamin Soltoff <ben@bensoltoff.com>
2017-02-16 14:55:58 -0600

    Add more helper functions

--- R/helper.R
+++ R/helper.R
@@ -7,3 +7,25 @@
 logit2prob <- function(x){
   exp(x) / (1 + exp(x))
 }
+
+
+#' Convert predicted probabilities to log-dds
+#'
+#' Function to convert predicted probabilities to log-odds.
+#'
+#' @param x A vector of predicted probabilities.
+#' @export
+prob2odds <- function(x){
+  x / (1 - x)
+}
+
+
+#' Convert probabilities to log-odds
+#'
+#' Function to convert probabilities to log-odds.
+#'
+#' @param x A vector of predicted probabilities.
+#' @export
+prob2logodds <- function(x){
+  log(prob2odds(x))
+}
