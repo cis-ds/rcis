63f9833b739b7fdd2431ec367358f78f44092ec8
Benjamin Soltoff <ben@bensoltoff.com>
2019-04-11 13:43:39 -0500

    Add default function to create xaringan slides

--- R/render.R
+++ R/render.R
@@ -32,12 +32,35 @@ cfss_notes <- function() {
                            toc_float = TRUE)
 }
 
-#' Default base size for `ggplot()` objects
+#' Default base size for \code{\link{ggplot}} objects
 #'
-#' Default base size for `ggplot()` objects intended to render in lecture slides.
+#' Default base size for \code{\link{ggplot}} objects intended to render in lecture slides.
 #'
 #' @format A numeric value
 #' @export
 "base_size"
 
 base_size <- 24
+
+#' xaringan
+#'
+#' Function to generate slides for class using \code{\link[xaringan]{moon_reader}}
+#'
+#'
+#' @importFrom magrittr "%>%"
+#' @export
+#'
+xaringan <- function(){
+  xaringan::moon_reader(
+    css = c("default", "metropolis", "lucy-fonts"),
+    lib_dir = "libs",
+    nature = list(
+      beforeInit = "https://cfss.uchicago.edu/slides/macros.js",
+      highlightLanguage = "r",
+      highlightStyle = "github",
+      highlightLines = TRUE,
+      countIncrementalSlides = FALSE
+    )
+  )
+}
+
