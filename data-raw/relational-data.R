d5a9754a51d7865b54dd3faab3bc462743e64a94
Benjamin Soltoff <ben@bensoltoff.com>
2019-04-17 09:19:41 -0500

    Update superheroes/publishers data to be reproducible. Change last row to Sabrina in superheroes

---
+++ data-raw/relational-data.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+
+superheroes <- "
+    name,    alignment, gender,         publisher
+ Magneto,          bad,   male,            Marvel
+   Storm,         good, female,            Marvel
+Mystique,          bad, female,            Marvel
+  Batman,         good,   male,                DC
+   Joker,          bad,   male,                DC
+Catwoman,          bad, female,                DC
+ Sabrina, questionable, female,     Archie Comics
+"
+
+superheroes <- read_csv(superheroes, skip = 1)
+
+publishers <- "
+  publisher, yr_founded
+         DC,       1934
+     Marvel,       1939
+      Image,       1992
+"
+publishers <- read_csv(publishers, skip = 1)
+
+usethis::use_data(superheroes, overwrite = TRUE)
+usethis::use_data(publishers, overwrite = TRUE)
