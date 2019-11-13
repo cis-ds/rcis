6dd8d8bec24de2149acf7151fe92f7a7d3410dde
Benjamin Soltoff <ben@bensoltoff.com>
2019-11-13 11:19:27 -0600

    Fix gun deaths education column. Not formatted properly

--- data-raw/gun-deaths.R
+++ data-raw/gun-deaths.R
@@ -13,9 +13,7 @@ gun_deaths <- gun_deaths_raw %>%
            factor(levels = 1:12, labels = month.abb) %>%
            as.character()) %>%
   # clean up education column
-  mutate(education = ifelse(education == 5, NA, education),
-         education = factor(education, levels = c(1:4),
-                            labels = c("Less than HS", "HS/GED", "Some college", "BA+")))
+  mutate(education = factor(education, c("Less than HS", "HS/GED", "Some college", "BA+")))
 
 # store in package
 usethis::use_data(gun_deaths, overwrite = TRUE)
