873bef8fdd703a94c9552dd0dfaf1c1b5b458f98
Benjamin Soltoff <ben@bensoltoff.com>
2020-09-05 13:11:47 -0500

    Ensure data frames are tibble formatted

--- data-raw/cps.R
+++ data-raw/cps.R
@@ -1,15 +1,19 @@
 library(tidyverse)
 library(RSocrata)
 
-#importing city of chicago report card 2018-2019 data using RSocrata
+# importing city of chicago report card 2018-2019 data using RSocrata
 reportcards <- read.socrata(
   "https://data.cityofchicago.org/Education/Chicago-Public-Schools-School-Progress-Reports-SY1/dw27-rash"
-)
-#growth rating: Student Growth measures the change in standardized test scores between two points in time compared other CPS school and schools nationwide. The score is based on an average of the available PSAT and SAT growth measures listed below.
+) %>%
+  as_tibble()
 
-qualvars <- c("school_type", "primary_category", "student_growth_rating", "student_attainment_rating", "culture_climate_rating",
-              "creative_school_certification", "school_survey_involved", "school_survey_supportive",
-              "school_survey_ambitious", "school_survey_effective", "school_survey_safety")
+# growth rating: Student Growth measures the change in standardized test scores between two points in time compared other CPS school and schools nationwide. The score is based on an average of the available PSAT and SAT growth measures listed below.
+
+qualvars <- c(
+  "school_type", "primary_category", "student_growth_rating", "student_attainment_rating", "culture_climate_rating",
+  "creative_school_certification", "school_survey_involved", "school_survey_supportive",
+  "school_survey_ambitious", "school_survey_effective", "school_survey_safety"
+)
 quantvars <- c("attainment_math_pct_es", "attainment_reading_pct_es", "student_attendance_year_2", "teacher_attendance_year_2", "chronic_truancy_pct")
 myvars <- c("school_id", "short_name", qualvars, quantvars)
 
@@ -27,7 +31,7 @@ cps <- reportcards[myvars] %>%
   na_if("NOT ENOUGH DATA") %>%
   na_if("INCOMPLETE DATA") %>%
   mutate(
-    student_growth_rating = factor (
+    student_growth_rating = factor(
       student_growth_rating,
       c(
         "FAR BELOW AVERAGE",
@@ -37,11 +41,11 @@ cps <- reportcards[myvars] %>%
         "FAR ABOVE AVERAGE"
       )
     ),
-    ambitious_teaching = factor (
+    ambitious_teaching = factor(
       ambitious_teaching,
       c("WEAK", "NEUTRAL", "STRONG", "VERY STRONG")
     ),
-    student_attainment_rating = factor (
+    student_attainment_rating = factor(
       student_attainment_rating,
       c(
         "FAR BELOW EXPECTATIONS",
@@ -74,7 +78,7 @@ cps <- reportcards[myvars] %>%
       involved_families,
       c("VERY WEAK", "WEAK", "NEUTRAL", "STRONG", "VERY STRONG")
     ),
-    supportive_environment = factor (
+    supportive_environment = factor(
       supportive_environment,
       c("VERY WEAK", "WEAK", "NEUTRAL", "STRONG", "VERY STRONG")
     ),
@@ -87,6 +91,5 @@ cps <- reportcards[myvars] %>%
       c("VERY WEAK", "WEAK", "NEUTRAL", "STRONG", "VERY STRONG")
     )
   )
-
 
 usethis::use_data(cps, overwrite = TRUE)
